99ec9af916aa3ecb2361aa4a861c4443a55f4307
Chris Wu <cwu0569@gmail.com>
2021-01-11 22:21:16 -0800

    shop card styling

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -2,30 +2,53 @@ import React, { useState, useEffect, useRef } from 'react';
 
 const ShopCard = ({ pic, hover, name }) => {
   let imgEl = useRef(null);
+
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
   const [imgWidth, setImgWidth] = useState(0);
   const [imgHeight, setImgHeight] = useState(0);
 
-  useEffect(() => {
+  const resizeImgs = () => {
     setImgWidth(imgEl.width);
     const ratio = 450 / 600;
     setImgHeight(imgWidth * ratio);
-  });
+  };
+
+  const handleResize = () => {
+    setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    resizeImgs();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [dimensions, imgWidth]);
 
   return (
     <div className="shop-card">
       <div className="shop-card-click">
-        <img
-          ref={(el) => {
-            imgEl = el;
-          }}
-          src={pic}
-          alt={name}
-          style={{ height: imgHeight }}
-          className="shop-img"
-        />
+        <div className="img-container">
+          <img
+            ref={(el) => {
+              imgEl = el;
+            }}
+            src={pic}
+            alt={name}
+            style={{ height: imgHeight }}
+            className="shop-img"
+          />
+          <div className="img-overlay">
+            <img src={hover} alt={name} style={{ height: imgHeight }} />
+          </div>
+        </div>
         <div className="shop-item-name">{name}</div>
       </div>
-      <div>12 bottles</div>
+      <div className="bottle-text">12 bottles</div>
     </div>
   );
 };
